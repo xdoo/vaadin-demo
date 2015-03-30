877a6455daa11e19f78989ceabb3439af25c5c95
claus.straube <claus.straube@emmerich.afd.dir.muenchen.de>
2015-03-30 14:29:02 +0200

    navigation demo

--- src/main/java/de/muenchen/vaadin/views/MyVaadinUI.java
+++ src/main/java/de/muenchen/vaadin/views/MyVaadinUI.java
@@ -5,21 +5,59 @@
  */
 package de.muenchen.vaadin.views;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
+import com.vaadin.spring.navigator.SpringViewProvider;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author claus.straube
  */
+@Theme("valo")
 @SpringUI
 public class MyVaadinUI extends UI {
+    
+    @Autowired
+    private SpringViewProvider viewProvider;
 
     @Override
     protected void init(VaadinRequest request) {
-        setContent(new Label("Hello! I'm the root UI!"));
+        final VerticalLayout root = new VerticalLayout();
+        root.setSizeFull();
+        root.setMargin(true);
+        root.setSpacing(true);
+        setContent(root);
+        
+        final CssLayout navigationBar = new CssLayout();
+        navigationBar.addStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+        navigationBar.addComponent(createNavigationButton("View 1", View1.VIEW_NAME));
+        navigationBar.addComponent(createNavigationButton("View 2", View2.VIEW_NAME));
+        root.addComponent(navigationBar);
+
+        final Panel viewContainer = new Panel();
+        viewContainer.setSizeFull();
+        root.addComponent(viewContainer);
+        root.setExpandRatio(viewContainer, 1.0f);
+
+        Navigator navigator = new Navigator(this, viewContainer);
+        navigator.addProvider(viewProvider);
+    }
+    
+    private Button createNavigationButton(String caption, final String viewName) {
+        Button button = new Button(caption);
+        button.addStyleName(ValoTheme.BUTTON_SMALL);
+        // If you didn't choose Java 8 when creating the project, convert this to an anonymous listener class
+        button.addClickListener(event -> getUI().getNavigator().navigateTo(viewName));
+        return button;
     }
     
 }
