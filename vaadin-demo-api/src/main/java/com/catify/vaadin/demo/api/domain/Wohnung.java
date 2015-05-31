032b3ad69557fffe14032df73d1659638085cfc6
xdoo <claus@claus-900X3G>
2015-05-31 17:36:17 +0200

    added some dto to api

---
+++ vaadin-demo-api/src/main/java/com/catify/vaadin/demo/api/domain/Wohnung.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.catify.vaadin.demo.api.domain;
+
+/**
+ *
+ * @author claus
+ */
+public class Wohnung extends BaseDto {
+    
+}
