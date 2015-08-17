3da6ace47590ea146f2b91cf3b4f040ad0e2e706
claus.straube <claus.straube@emmerich.itm.muenchen.de>
2015-08-17 16:29:09 +0200

    Erster Entwurf einer Grid Komponente.

---
+++ vadin-demo-gui/src/main/java/de/muenchen/vaadin/ui/components/BuergerGrid.java
@@ -0,0 +1,66 @@
+package de.muenchen.vaadin.ui.components;
+
+import com.vaadin.data.Item;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.util.GeneratedPropertyContainer;
+import com.vaadin.data.util.PropertyValueGenerator;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Grid;
+import de.muenchen.vaadin.demo.api.domain.Buerger;
+import de.muenchen.vaadin.ui.controller.BuergerViewController;
+import de.muenchen.vaadin.ui.util.VaadinUtil;
+
+/**
+ *
+ * @author claus.straube
+ */
+public class BuergerGrid extends CustomComponent {
+
+    private final BeanItemContainer<Buerger> container;
+    private BuergerViewController controller;
+    private Grid grid;
+
+    public BuergerGrid(final BuergerViewController controller) {
+
+        this.controller = controller;
+        
+        // Have a container of some type to contain the data
+        this.container = new BeanItemContainer<>(Buerger.class);
+
+        this.grid = new Grid();
+        this.grid.setContainerDataSource(this.container);
+
+                // Generate button caption column
+        GeneratedPropertyContainer gpc
+                = new GeneratedPropertyContainer(this.container);
+        gpc.addGeneratedProperty(VaadinUtil.TABLE_COLUMN_ACTIONS,
+                new PropertyValueGenerator<String>() {
+
+                    @Override
+                    public String getValue(Item item, Object itemId,
+                            Object propertyId) {
+                        return ""; // The caption
+                    }
+
+                    @Override
+                    public Class<String> getType() {
+                        return String.class;
+                    }
+                });
+        
+        // configure
+        this.grid.setWidth("100%");
+        this.grid.setHeightByRows(10);
+        this.grid.getColumns().stream().forEach(c -> {c.setHidable(true);});
+        this.grid.setColumnOrder(Buerger.VORNAME, Buerger.NACHNAME, Buerger.GEBURTSDATUM, VaadinUtil.TABLE_COLUMN_ACTIONS);
+        
+        // set headers
+        this.grid.getColumn(Buerger.VORNAME).setHeaderCaption(controller.getMsg().readColumnHeader(controller.getI18nBasePath(), Buerger.VORNAME));
+        this.grid.getColumn(Buerger.GEBURTSDATUM).setHeaderCaption(controller.getMsg().readColumnHeader(controller.getI18nBasePath(), Buerger.GEBURTSDATUM));
+        this.grid.getColumn(Buerger.NACHNAME).setHeaderCaption(controller.getMsg().readColumnHeader(controller.getI18nBasePath(), Buerger.NACHNAME));
+        this.grid.getColumn(VaadinUtil.TABLE_COLUMN_ACTIONS).setHeaderCaption("");
+        
+        setCompositionRoot(this.grid);
+    }
+
+}
