721afedf22fb0fd3266328a3eb5e6bd8bdcf67f9
maximilian.schug <maximilian.schug@wks00005038.itm.muenchen.de>
2015-08-17 08:32:38 +0200

    moved resources from service to api

---
+++ vaadin-demo-api/src/main/java/de/muenchen/vaadin/demo/api/rest/PermissionResource.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.muenchen.vaadin.demo.api.rest;
+
+/**
+ *
+ * @author praktikant.tmar
+ */
+public class PermissionResource extends SecurityResource {
+    private String permision;
+
+    public String getPermision() {
+        return permision;
+    }
+
+    public void setPermision(String permision) {
+        this.permision = permision;
+    }
+
+}
