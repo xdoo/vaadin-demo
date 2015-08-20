a63bd9b228017910b7b5f44b90d8ff25684fb8bd
claus.straube <claus.straube@emmerich.itm.muenchen.de>
2015-08-20 16:48:34 +0200

    Cache einbauen (noch nicht abgeschlossen). see #55

--- vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/domain/BuergerRepository.java
+++ vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/domain/BuergerRepository.java
@@ -1,13 +1,25 @@
 package de.muenchen.demo.service.domain;
 
 import java.util.List;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 
 /**
  *
  * @author claus.straube
  */
 public interface BuergerRepository extends BaseRepository<Buerger> {
+    
+    public final static String BUERGER_CACHE = "BUERGER_CACHE";
+    
+    @Override
+    @Cacheable(value = BUERGER_CACHE, key = "#p0 + #p1")
+    public List<Buerger> findByOidAndMandantOid(String oid, String mid);
 
+    @Override
+    @CachePut(value = BUERGER_CACHE, key = "#p0.oid + #p0.mandant")
+    public Buerger save(Buerger entity);
+    
     List<Buerger> findByOid(String oid);
 
     List<Buerger> findByKinderOid(String oid);
