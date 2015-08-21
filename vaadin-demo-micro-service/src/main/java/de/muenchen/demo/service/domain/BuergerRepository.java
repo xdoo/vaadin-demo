6c012dbef55db1ed1d3d09a950605eaa1b45ed83
claus.straube <claus.straube@emmerich.itm.muenchen.de>
2015-08-21 07:38:55 +0200

    Cache wurde für Bürger eingebaut. close #55

--- vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/domain/BuergerRepository.java
+++ vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/domain/BuergerRepository.java
@@ -1,6 +1,7 @@
 package de.muenchen.demo.service.domain;
 
 import java.util.List;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 
@@ -12,13 +13,16 @@ public interface BuergerRepository extends BaseRepository<Buerger> {
     
     public final static String BUERGER_CACHE = "BUERGER_CACHE";
     
-    @Override
     @Cacheable(value = BUERGER_CACHE, key = "#p0 + #p1")
-    public List<Buerger> findByOidAndMandantOid(String oid, String mid);
+    public Buerger findFirstByOidAndMandantOid(String oid, String mid);
 
     @Override
     @CachePut(value = BUERGER_CACHE, key = "#p0.oid + #p0.mandant.oid")
     public Buerger save(Buerger entity);
+
+    @Override
+    @CacheEvict(value = BUERGER_CACHE, key = "#p0.oid + #p0.mandant.oid")
+    public void delete(Buerger entity);
     
     List<Buerger> findByOid(String oid);
 
--- vaadin-demo-micro-service/src/test/java/de/muenchen/demo/test/service/BuergerServiceTest.java
+++ vaadin-demo-micro-service/src/test/java/de/muenchen/demo/test/service/BuergerServiceTest.java
@@ -1,11 +1,14 @@
 package de.muenchen.demo.test.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.collect.Lists;
 import de.muenchen.demo.service.Application;
 import de.muenchen.demo.service.domain.Buerger;
 import de.muenchen.demo.service.domain.BuergerRepository;
 import de.muenchen.demo.service.services.BuergerService;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,32 +38,41 @@ public class BuergerServiceTest {
         System.out.println("========== save cache Test ==========");
         Cache cache = cacheManager.getCache(BuergerRepository.BUERGER_CACHE);
         Buerger b1 = this.createBuerger("OIDC00");
-        Buerger b2 = cache.get(b1.getOid() + b1.getMandant().getOid(), Buerger.class);
-        assertNotNull(b2);
+        assertNull(cache.get(b1.getOid() + "2", Buerger.class));
+        service.save(b1);
+        assertNotNull(cache.get(b1.getOid() + b1.getMandant().getOid(), Buerger.class));
+        System.out.println(String.format("Objekt wurde beim Speichern mit der ID '%s' in den Cache gelegt.", b1.getOid() + b1.getMandant().getOid()));
     }
     
     @Test @WithMockUser(username = "hans")
     public void testCacheOnRead() {
         System.out.println("========== read cache Test ==========");
         Cache cache = cacheManager.getCache(BuergerRepository.BUERGER_CACHE);
-        Buerger b1 = service.read("OID2");
-        Buerger b2 = cache.get(b1.getOid() + b1.getMandant().getOid(), Buerger.class);
-        assertNotNull(b2);
+        assertNull(cache.get("OID52"));
+        Buerger b1 = service.read("OID5");
+        assertNotNull(cache.get(b1.getOid() + b1.getMandant().getOid(), Buerger.class));
+        assertEquals("OID52", b1.getOid() + b1.getMandant().getOid());
+        System.out.println(String.format("Objekt wurde beim Lesen mit der ID '%s' in den Cache gelegt.", b1.getOid() + b1.getMandant().getOid()));
     }
     
     @Test @WithMockUser(username = "hans")
     public void createTest() throws JsonProcessingException {
         System.out.println("========== create Bürger Test ==========");
-        Buerger a = service.create();
-        assertNotEquals(null, a.getOid());
+        Buerger b1 = service.create();
+        assertNotNull(b1);
+        assertNotNull(b1.getOid());
+        assertNull(repo.findFirstByOidAndMandantOid(b1.getOid(), "2"));
+        System.out.println(String.format("Bürger wurde mit neuer OID '%s' erstellt (und nicht in der DB gespeichert)", b1.getOid()));
     }
 
     @Test @WithMockUser(username = "hans")
     public void saveTest() {
         System.out.println("========== save Bürger Test ==========");
-        Buerger a = service.save(this.createBuerger("OID0"));
-        assertEquals("OID0", a.getOid());
-        assertEquals("2", a.getMandant().getOid());
+        Buerger b1 = service.save(this.createBuerger("OID0"));
+        assertEquals("OID0", b1.getOid());
+        assertEquals("2", b1.getMandant().getOid());
+        assertNotNull(repo.findFirstByOidAndMandantOid("OID0", "2"));
+        System.out.println(String.format("Bürger wurde mit Mandant '%s' in der DB gespeichert.", b1.getMandant().getOid()));
     }
     
     private Buerger createBuerger(String oid) {
@@ -77,11 +89,9 @@ public class BuergerServiceTest {
         Buerger b1 = service.read("OID1");
         b1.setVorname("dagobert");
         service.update(b1);
-        
-        // check
         Buerger b2 = service.read("OID1");
         assertEquals("dagobert", b2.getVorname());
-
+        System.out.println("Bürger wurde mit neuem Vornamen in der DB gespeichert.");
     }
 
     @Test @WithMockUser(username = "hans")
@@ -89,6 +99,7 @@ public class BuergerServiceTest {
         System.out.println("========== read Bürger Test ==========");
         Buerger b1 = service.read("OID2");
         assertNotNull(b1);
+        System.out.println(String.format("Bürger konnte mit OID '%s' aus der DB gelesen werden.", b1.getOid()));
     }
 
     @Test @WithMockUser(username = "hans")
@@ -99,13 +110,30 @@ public class BuergerServiceTest {
         service.delete(b1.getOid());
         Buerger b2 = service.read("OID3");
         assertNull(b2);
+        System.out.println(String.format("Bürger konnte mit OID '%s' aus der DB (und dem Cache) gelöscht werden.", b1.getOid()));
     }
 
     @Test @WithMockUser(username = "hans")
-    public void queryTest() {
-        long num = repo.count();
+    public void queryTestM2() {
+        System.out.println("========== query Bürger Test Mandant 2 ==========");
+        int x = this.count("2");
         List<Buerger> bs = service.query();
-        assertEquals(num, bs.size());
+        assertEquals(x, bs.size());
+        System.out.println(String.format("Suche wurde erfolgreich durchgeführt. Einträge Mandant 2: %s | Ergebnis der Suche: %s", x, bs.size()));
+    }
+    
+    @Test @WithMockUser(username = "franz")
+    public void queryTestM3() {
+        System.out.println("========== query Bürger Test Mandant 3 ==========");
+        int x = this.count("3");
+        List<Buerger> bs = service.query();
+        assertEquals(x, bs.size());
+        System.out.println(String.format("Suche wurde erfolgreich durchgeführt. Einträge Mandant 3: %s | Ergebnis der Suche: %s", x, bs.size()));
+    }
+    
+    private int count(String mid) {
+        ArrayList<Buerger> all = Lists.newArrayList(repo.findAll());
+        return all.stream().filter(b -> b.getMandant().getOid().equals(mid)).collect(Collectors.counting()).intValue();
     }
 
     @Test @WithMockUser(username = "hans")
@@ -115,5 +143,8 @@ public class BuergerServiceTest {
         assertNotNull(b1);
         Buerger b2 = service.read(b1.getOid());
         assertNotNull(b2);
+        assertEquals(b1.getVorname(), b2.getVorname());
+        assertEquals(b1.getNachname(), b2.getNachname());
+        System.out.println(String.format("Objekt mit der OID '%s' konnte erfolgreich in Objekt '%s' kopiert (und in DB gespeichert) werden", "OID4", b2.getOid()));
     }
 }
