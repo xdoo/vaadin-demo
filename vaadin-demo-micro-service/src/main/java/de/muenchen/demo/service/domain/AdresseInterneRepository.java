bec3a9c72edbf66c8cfed7c8ad26ba57fed70abe
Peter Müller <p.mueller@muenchen.de>
2015-09-11 10:38:42 +0200

    all repos with mandant
    #66

--- vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/domain/AdresseInterneRepository.java
+++ vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/domain/AdresseInterneRepository.java
@@ -1,32 +1,48 @@
 package de.muenchen.demo.service.domain;
 
+import de.muenchen.demo.service.security.TenantService;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 
-/**
- *
- * @author claus.straube
- */
+@RepositoryRestResource(exported = true)
 @PreAuthorize("hasRole('PERM_READ_AdresseInterne')")
 public interface AdresseInterneRepository extends CrudRepository<AdresseInterne, Long> {
 
-    @Override
-    @PreAuthorize("hasRole('PERM_WRITE_AdresseInterne')")
-    AdresseInterne save(AdresseInterne entity);
+	@Override
+	@PostFilter(TenantService.IS_TENANT_FILTER)
+	Iterable<AdresseInterne> findAll();
 
-    @Override
-    @PreAuthorize("hasRole('PERM_DELETE_AdresseInterne')")
-    void delete(Long aLong);
+	@Override
+	@PreAuthorize("hasRole('PERM_READ_AdresseInterne')")
+	@PostAuthorize(TenantService.IS_TENANT_AUTH)
+	AdresseInterne findOne(Long aLong);
 
-    @Override
-    @PreAuthorize("hasRole('PERM_DELETE_AdresseInterne')")
-    void delete(AdresseInterne adresseInterne);
+	@Override
+	@PreAuthorize("hasRole('PERM_WRITE_AdresseInterne')")
+	AdresseInterne save(AdresseInterne AdresseInterne);
 
-    @Override
-    @PreAuthorize("hasRole('PERM_DELETE_AdresseInterne')")
-    void delete(Iterable<? extends AdresseInterne> iterable);
+	@Override
+	@PreAuthorize("hasRole('PERM_DELETE_AdresseInterne')")
+	@PostAuthorize(TenantService.IS_TENANT_AUTH)
+	void delete(Long aLong);
 
-    @Override
-    @PreAuthorize("hasRole('PERM_DELETE_AdresseInterne')")
-    void deleteAll();
+	@Override
+	@PreAuthorize("hasRole('PERM_DELETE_AdresseInterne')")
+	@PreFilter(TenantService.IS_TENANT_FILTER)
+	void delete(Iterable<? extends AdresseInterne> iterable);
+
+	@Override
+	@PreAuthorize("hasRole('PERM_DELETE_AdresseInterne')")
+	@PreFilter(TenantService.IS_TENANT_FILTER)
+	void deleteAll();
+
+	@Override
+	@PreAuthorize("hasRole('PERM_DELETE_AdresseInterne')")
+	@PostAuthorize(TenantService.IS_TENANT_AUTH)
+	void delete(AdresseInterne entity);
+	
 }
