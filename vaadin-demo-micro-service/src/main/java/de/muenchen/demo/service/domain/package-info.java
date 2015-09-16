869893b37740db88deb34a7ea8d12d2044973dd1
Peter Müller <p.mueller@muenchen.de>
2015-09-16 10:59:32 +0200

    Travis fix (leere java klassen gelöscht) #109

--- vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/domain/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Configure full text search filters for domain classes.
- */
-@FullTextFilterDef(name = "tenantSearchFilter", impl = TenantSearchFilterFactory.class)
-package de.muenchen.demo.service.domain;
-
-import org.hibernate.search.annotations.FullTextFilterDef;
-import de.muenchen.demo.service.util.TenantSearchFilterFactory;
