12ecf45d8ec571d634542b1e17be047d722ffbb4
Rene Zarwel <rene.zarwel@muenchen.de>
2015-09-25 11:52:16 +0200

    Find Link wird angehangen - Noch ist Link falsch

--- vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/rest/search/BuergerSearchController.java
+++ vaadin-demo-micro-service/src/main/java/de/muenchen/demo/service/rest/search/BuergerSearchController.java
@@ -3,18 +3,20 @@ package de.muenchen.demo.service.rest.search;
 import de.muenchen.demo.service.domain.Buerger;
 import de.muenchen.demo.service.util.QueryService;
 import org.hibernate.search.annotations.IndexedEmbedded;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.PersistentEntityResource;
 import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
-import org.springframework.data.rest.webmvc.RepositorySearchesResource;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,8 +31,11 @@ import java.util.stream.Stream;
  * Created by p.mueller on 24.09.15.
  */
 @BasePathAwareController
+@Component
 @RequestMapping("/buergers")
-public class BuergerSearchController implements ResourceProcessor<RepositorySearchesResource> {
+public class BuergerSearchController implements ResourceProcessor<Resources<Buerger>> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BuergerSearchController.class);
 
     @Autowired
     QueryService service;
@@ -58,9 +63,9 @@ public class BuergerSearchController implements ResourceProcessor<RepositorySear
     }
 
     @Override
-    public RepositorySearchesResource process(RepositorySearchesResource resource) {
+    public Resources<Buerger> process(Resources<Buerger> resource) {
         Link link = new Link(resource.getLink("self") + "/find", "find");
-        System.out.println("ASDF: " + link.getHref());
+        LOG.error("ASDF: " + link.getHref());
         resource.add(link);
         return resource;
     }
