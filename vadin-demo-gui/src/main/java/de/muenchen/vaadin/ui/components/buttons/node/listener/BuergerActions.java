5ac2f0e587bc1b7a5488391ed30a8542b2307ed8
Rene Zarwel <rene.zarwel@muenchen.de>
2015-10-15 11:40:55 +0200

    GenericGrid an Actions angepasst #158

--- vadin-demo-gui/src/main/java/de/muenchen/vaadin/ui/components/buttons/node/listener/BuergerActions.java
+++ vadin-demo-gui/src/main/java/de/muenchen/vaadin/ui/components/buttons/node/listener/BuergerActions.java
@@ -4,12 +4,14 @@ import de.muenchen.eventbus.EventBus;
 import de.muenchen.vaadin.demo.api.local.Buerger;
 import de.muenchen.vaadin.guilib.components.actions.EntityActions;
 
+import java.util.function.Supplier;
+
 /**
  * Created by p.mueller on 09.10.15.
  */
 public class BuergerActions extends EntityActions {
 
-    public BuergerActions(EventBus eventBus) {
-        super(eventBus, Buerger.class);
+    public BuergerActions(Supplier<String> filterSupplier, EventBus eventBus) {
+        super(filterSupplier, eventBus, Buerger.class);
     }
 }
