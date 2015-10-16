e1ac7560563a78fb7b05f238a6a25b2522e2d080
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2015-10-16 07:52:02 +0200

    @arne2 und @darenegade glücklich machen

--- vadin-demo-gui/src/main/java/de/muenchen/vaadin/services/BuergerServiceImpl.java
+++ vadin-demo-gui/src/main/java/de/muenchen/vaadin/services/BuergerServiceImpl.java
@@ -23,7 +23,13 @@ import org.springframework.web.client.RestTemplate;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static de.muenchen.vaadin.demo.i18nservice.I18nPaths.getNotificationPath;
 
