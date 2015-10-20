d3bf04725b390c1203ac61d9bdee1b3d9147d796
Rene Zarwel <rene.zarwel@muenchen.de>
2015-10-20 17:20:06 +0200

    Etwas Magie und er nutzt den JWT Token #93

--- Security/src/main/java/de/muenchen/security/OAuth2AuthorizationConfig.java
+++ Security/src/main/java/de/muenchen/security/OAuth2AuthorizationConfig.java
@@ -1,12 +1,18 @@
 package de.muenchen.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.security.KeyPair;
 
 /**
  * Created by rene.zarwel on 20.10.15.
@@ -19,9 +25,20 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    //TODO GET A GOOD KEYSTORE
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        KeyPair keyPair = new KeyStoreKeyFactory(
+                new ClassPathResource("server.jks"), "123456".toCharArray())
+                .getKeyPair("mytestkey", "123456".toCharArray());
+        converter.setKeyPair(keyPair);
+        return converter;
+    }
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
     }
 
     @Override
