3e71de4f1662527a40dff755f9ceef9b7edbb7aa
Peter Müller <p.mueller@muenchen.de>
2015-10-26 16:19:42 +0100

    activateEdit in grid wieder eingefügt

--- vadin-demo-gui/src/main/java/de/muenchen/vaadin/ui/components/BuergerGrid.java
+++ vadin-demo-gui/src/main/java/de/muenchen/vaadin/ui/components/BuergerGrid.java
@@ -4,6 +4,7 @@ import de.muenchen.vaadin.demo.api.local.Buerger;
 import de.muenchen.vaadin.guilib.components.GenericGrid;
 import de.muenchen.vaadin.ui.app.views.BuergerCreateView;
 import de.muenchen.vaadin.ui.app.views.BuergerDetailView;
+import de.muenchen.vaadin.ui.app.views.BuergerUpdateView;
 import de.muenchen.vaadin.ui.controller.BuergerViewController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +27,7 @@ public class BuergerGrid extends GenericGrid {
                 .activateDoubleClickToRead(BuergerDetailView.NAME)
                 .activateCreate(BuergerCreateView.NAME)
                 .activateSearch()
+                .activateEdit(BuergerUpdateView.NAME)
                 .activateRead(BuergerDetailView.NAME)
                 .activateCopy()
                 .activateDelete();
