275838ab2a6cb6f6fbbc4f9df95f50a5d2bd1972
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2015-10-28 13:06:16 +0100

    kommentatiiert

--- Service-Lib/src/main/java/de/muenchen/service/DatabaseBeforeSaveEventListener.java
+++ Service-Lib/src/main/java/de/muenchen/service/DatabaseBeforeSaveEventListener.java
@@ -5,9 +5,15 @@ import javax.persistence.PrePersist;
 /**
  * Created by claus.straube on 28.10.15.
  * fabian.holtkoetter ist unschuldig.
+ *
+ * Wird von BaseEntity verwendet, um das setzen der OID einer Entität vor dem Speichern in der Datenbank zu ermöglichen.
  */
 public class DatabaseBeforeSaveEventListener {
 
+    /**
+     * Setzt die OID der zu speichernden Entität vor dem Speichern in der Datenbank
+     * @param object
+     */
     @PrePersist
     public void addOid(Object object) {
         BaseEntity entity = (BaseEntity) object;
