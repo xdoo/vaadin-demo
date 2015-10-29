5b91f17d80c514b24f0d39587b864e07c8483914
Peter Müller <p.mueller@muenchen.de>
2015-10-29 15:29:30 +0100

    dash

--- vadin-demo-gui/src/main/java/de/muenchen/vaadin/ui/components/forms/BuergerPartnerForm.java
+++ vadin-demo-gui/src/main/java/de/muenchen/vaadin/ui/components/forms/BuergerPartnerForm.java
@@ -84,9 +84,9 @@ public class BuergerPartnerForm extends BaseComponent {
     private void setIds() {
         setId(getClass().getSimpleName());
         getPartnerForm().setId(getId() + "#form");
-        getCreateButton().setId(getId() + "#create-button" + getCreateNavigation().getNavigateTo());
-        getAddButton().setId(getId() + "#add-button" + getAddNavigation().getNavigateTo());
-        getReadButton().setId(getId() + "#read-button" + getReadNavigation().getNavigateTo());
+        getCreateButton().setId(getId() + "#create-button-" + getCreateNavigation().getNavigateTo());
+        getAddButton().setId(getId() + "#add-button-" + getAddNavigation().getNavigateTo());
+        getReadButton().setId(getId() + "#read-button-" + getReadNavigation().getNavigateTo());
         getDeleteButton().setId(getId() + "#delete-button");
     }
 
