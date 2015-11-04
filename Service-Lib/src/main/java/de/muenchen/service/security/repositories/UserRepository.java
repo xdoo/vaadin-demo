334f56ed9fbf77f3470038406e70d16fa077d67c
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2015-11-04 10:09:14 +0100

    LDAP holt sich die Rollen jetzt per Query aus der Datenbank

--- Service-Lib/src/main/java/de/muenchen/service/security/repositories/UserRepository.java
+++ Service-Lib/src/main/java/de/muenchen/service/security/repositories/UserRepository.java
@@ -5,33 +5,75 @@
  */
 package de.muenchen.service.security.repositories;
 
+import de.muenchen.service.TenantService;
 import de.muenchen.service.security.entities.User;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 
 /**
- *
  * @author praktikant.tmar
  */
+@PreAuthorize("hasRole('ROLE_READ_SEC_User')")
+@EnableOAuth2Resource
 @RepositoryRestResource
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    public final static String User_CACHE = "USER_CACHE";
+    String User_CACHE = "SEC_USER_CACHE";
 
+
+    @PostAuthorize(TenantService.IS_TENANT_AUTH)
     @Cacheable(value = User_CACHE, key = "#p0")
-    public User findFirstByOid(String id);
+    public User findFirstByUsername(@Param("username") String username);
 
     @Override
+    @PostFilter(TenantService.IS_TENANT_FILTER)
+    Iterable<User> findAll();
+
+    @Override
+    @Cacheable(value = User_CACHE, key = "#p0")
+
+    @PostAuthorize(TenantService.IS_TENANT_AUTH)
+    User findOne(Long oid);
+
+    @SuppressWarnings("unchecked")
+    @Override
     @CachePut(value = User_CACHE, key = "#p0.oid")
-    public User save(User entity);
+    @PreAuthorize("hasRole('ROLE_WRITE_SEC_User')")
+    User save(User User);
+
+    @Override
+    @CacheEvict(value = User_CACHE, key = "#p0")
+    @PreAuthorize("hasRole('ROLE_DELETE_SEC_User')")
+    @PostAuthorize(TenantService.IS_TENANT_AUTH)
+    void delete(Long oid);
+
 
     @Override
     @CacheEvict(value = User_CACHE, key = "#p0.oid")
-    public void delete(User entity);
+    @PreAuthorize("hasRole('ROLE_DELETE_SEC_User')")
+    @PostAuthorize(TenantService.IS_TENANT_AUTH)
+    void delete(User entity);
 
-    public User findFirstByUsername(@Param("username") String username);
+    @Override
+    @CacheEvict(value = User_CACHE, allEntries = true)
+    @PreAuthorize("hasRole('ROLE_DELETE_SEC_User')")
+    @PreFilter(TenantService.IS_TENANT_FILTER)
+    void delete(Iterable<? extends User> entities);
+
+
+    @Override
+    @CacheEvict(value = User_CACHE, allEntries = true)
+    @PreAuthorize("hasRole('ROLE_DELETE_SEC_User')")
+    @PreFilter(TenantService.IS_TENANT_FILTER)
+    void deleteAll();
+
 }
