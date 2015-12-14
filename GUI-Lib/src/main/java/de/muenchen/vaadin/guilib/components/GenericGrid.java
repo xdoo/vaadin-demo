06e0daad4f3bfe26f3523dffb2f6c25cc7eb16fb
Rene Zarwel <rene.zarwel@muenchen.de>
2015-12-14 14:07:10 +0100

    Methode erstellt #300

--- GUI-Lib/src/main/java/de/muenchen/vaadin/guilib/components/GenericGrid.java
+++ GUI-Lib/src/main/java/de/muenchen/vaadin/guilib/components/GenericGrid.java
@@ -649,6 +649,12 @@ public class GenericGrid<T> extends BaseComponent {
         return this;
     }
 
+    /**
+     * Deselects all selected Items.
+     */
+    public void deselectAll(){
+        grid.getSelectedRows().forEach(grid::deselect);
+    }
     //--------------
     // Getter / Setter
     //--------------
