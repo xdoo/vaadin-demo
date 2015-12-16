671390f3eb50a8f149b09d29b5fbd9e3371adf67
huningd <flipdeskript@gmail.com>
2015-12-16 16:40:11 +0100

    Echo service returns principal and request

--- echo-service/src/main/groovy/de/muenchen/demo/echo/EchoApplication.groovy
+++ echo-service/src/main/groovy/de/muenchen/demo/echo/EchoApplication.groovy
@@ -3,6 +3,7 @@ package de.muenchen.demo.echo
 import org.apache.coyote.Request
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 import javax.servlet.http.HttpServletRequest
+import java.security.Principal
 
 /**
  * Created by huningd on 15.12.15.
@@ -20,8 +22,8 @@ import javax.servlet.http.HttpServletRequest
 class EchoApplication {
 
     @RequestMapping('/')
-    def home(@RequestBody String msg) {
-       msg
+    def home(@RequestBody String msg, Authentication principal) {
+        ['name':principal.getName(), 'authorities':principal.getAuthorities(), 'request': msg]
     }
 
     static void main(String[] args) {
