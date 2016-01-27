0c3ad5f9c78956975d2a6f966f285444c42fe7d6
huningd <flipdeskript@gmail.com>
2016-01-27 16:32:00 +0100

    Added DicoveryClient; Resolve UserInfoSerivce with ribbon; Authorisation over UserInfoService;

--- echo-service/src/main/groovy/de/muenchen/demo/echo/EchoApplication.groovy
+++ echo-service/src/main/groovy/de/muenchen/demo/echo/EchoApplication.groovy
@@ -1,11 +1,24 @@
 package de.muenchen.demo.echo
 
+import com.netflix.discovery.DiscoveryClient
+import de.muenchen.service.security.OAuth2RestTemplateConfig
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.client.OAuth2RestTemplate
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -21,6 +34,8 @@ EnableGlobalAuthentication, EnableGlobalMethodSecurity, EnableWebSecurity, or En
 Hence you must separate it in different classes. Otherwise it will leeds to a class build cycle.
  */
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableEurekaClient
+@Import(OAuth2RestTemplateConfig.class)
 class EchoApplication {
 
     @RestController
@@ -31,7 +46,7 @@ class EchoApplication {
         }
 
         @RequestMapping('/secret')
-        @PreAuthorize("hasRole('ECHO_SECRET')")
+        @PreAuthorize("hasRole('READ_SEC_User')")
         def echoSecret(@RequestBody String msg, Authentication principal) {
             ['name': principal.getName(), 'authorities': principal.getAuthorities(), 'request': msg]
         }
